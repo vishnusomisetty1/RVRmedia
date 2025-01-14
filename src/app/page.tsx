2658ad53de2c96342e980feec54dbf8176e4bf46
vishnusomisetty1 <Vishnusomisetty14@gmail.com>
2025-01-13 21:47:33 -0500

    removed stream

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Services from './components/Services';
-import Stream from './components/Stream';
 import Team from './components/Team';
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
-      <Stream />
       <Services />
       <Team />
     </>
