680f8b190cd00855d5e2d2590500c91135f6617d
vishnusomisetty1 <Vishnusomisetty14@gmail.com>
2026-05-23 15:06:11 -0400

    moved gallery from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import Hero from './components/Hero';
 import Services from './components/Services';
 import Team from './components/Team';
-import Gallery from './components/Gallery';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Services />
-      <Gallery />
       <Team />
     </>
   );
